a0c5a00fcf08f193dd217ed88ba2d2dc09a771bd
Eugene <5867486@gmail.com>
2017-07-31 09:06:06 +0300

    for heroku

--- api/services/SocketService.js
+++ api/services/SocketService.js
@@ -4,6 +4,15 @@ var app = require('http').createServer();
 var io = require('socket.io')(app);
 app.listen(process.env.PORT || 3000);
 
+
+// var io = require('socket.io-client');
+var socket = io.connect('https://calm-earth-42681.herokuapp.com/',
+    {reconnect: true, transports : ['websocket'], path: '/socket.io'});
+
+socket.on('connect', function (socket) {
+    console.log('Connected!');
+});
+
 io.on('connection', function (socket) {
 
     console.log('Socket id is : ', socket.id);
@@ -15,6 +24,10 @@ io.on('connection', function (socket) {
     });
 
 });
+
+
+
+
 
 module.exports = {
 
