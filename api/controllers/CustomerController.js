b5e3a452961084bd05737fea151bd634e62646d2
Eugene <5867486@gmail.com>
2017-08-02 10:26:56 +0300

    add servise ( comments)

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -37,6 +37,76 @@ module.exports = {
                 return next();
             }
 
+
+            //-------------------------------------------------
+            //
+            // var http = require('http');
+            //
+            // function process_response(webservice_response, stock, callback) {
+            //
+            //     var webservice_data = '';
+            //
+            //     webservice_response.on('error', function (e) {
+            //        console.log(e.message);
+            //        callback("Error: " + e.message);
+            //     });
+            //
+            //     webservice_response.on('data', function (chunk) {
+            //         webservice_data += chunk;
+            //     });
+            //
+            //     webservice_response.on('end', function () {
+            //         stock_data = JSON.parse(webservice_data);
+            //         stock.current_price = stock_data.LastPrice;
+            //         console.log(stock.symbol + ' = $' + stock.current_price);
+            //         callback();
+            //     });
+            //
+            // };
+            //
+            // function get_current_price(stock, callback) {
+            //
+            //     options = {
+            //         post: 'dev.markitondemand.com',
+            //         port:80,
+            //         path:'/MODApis/Api/v2/Quote/JSON?symbol=' + stock.symbol,
+            //         method: 'GET'
+            //     };
+            //
+            //     var webservice_request = http.request(options, function (response) {
+            //         process_response(response, stock, callback);
+            //     });
+            //     webservice_request.end();
+            //
+            //     // console.log(stock.symbol + ' = ' + stock.current_price);
+            //
+            // }
+            //
+            // async.each(customer.stocks, get_current_price, function (err) {
+            //     if(err){
+            //         console.log(err);
+            //         // console.log('done');
+            //
+            //
+            //         res.view({
+            //             customer: customer
+            //         });
+            //     }
+            //
+            // });
+            // customer.stocks.forEach(function (stock) {
+            //    get_current_price(stock);
+            // });
+
+
+            //-------------------------------------------------
+
+
+
+
+
+
+
             res.view({
                 customer: customer
             });
